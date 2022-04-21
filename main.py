4591a5e3b2ce78fa9c2a067e99750e515cdd8074
Nborodin86 <nborodin86@gmail.com>
2022-04-21 21:32:35 +0700

    python 3.9 tools

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QIcon
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("vPLC")
+        self.setMinimumSize(800,600)
+        self.setWindowIcon(QIcon('Graphics/ico.png'))
+        self.statusBar()
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
+
